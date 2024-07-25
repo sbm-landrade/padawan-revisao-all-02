25d29becda3289a51105f3034e153b1eef50fec7
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-24 22:33:33 -0300

    Alteração banco de dados

--- filme/src/main/resources/data.sql
+++ filme/src/main/resources/data.sql
@@ -1,4 +1,11 @@
+CREATE TABLE filme (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255),
+    diretor VARCHAR(255),
+    genero VARCHAR(255),
+    ano INT
+);
+
 INSERT INTO filme (titulo, diretor, genero, ano) VALUES ('O Poderoso Chefão', 'Francis Ford Coppola', 'Drama', 1972);
 INSERT INTO filme (titulo, diretor, genero, ano) VALUES ('Os Bons Companheiros', 'Martin Scorsese', 'Crime', 1990);
-INSERT INTO filme (titulo, diretor, genero, ano) VALUES ('
-Vingadores: Ultimato', 'Irmãos russos', 'Ação', 2019);
+INSERT INTO filme (titulo, diretor, genero, ano) VALUES ('Vingadores: Ultimato', 'Irmãos Russo', 'Ação', 2019);
